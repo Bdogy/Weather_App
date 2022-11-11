bb9ba5cd6b548d6f1184c99e2236c6465b0184e0
Jackson Grimm <92557509+Bdogy@users.noreply.github.com>
2022-11-10 20:02:18 -0700

    created function to pull data for the city lat and lon

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,29 @@
 var userInput = $("#citySearch");
 var formButton = $("#submit");
 var url =
-  "api.openweathermap.org/data/2.5/forecast?lat=10&lon=10&appid=569d785adfe9b44db482c835162b2e7a";
-var urlLatLon =
-  "http://api.openweathermap.org/geo/1.0/direct?q=London&limit=5&appid=569d785adfe9b44db482c835162b2e7a";
+  "api.openweathermap.org/data/2.5/forecast?lat=51.5073219&lon=-0.1276474&units=imperial&appid=569d785adfe9b44db482c835162b2e7a";
+
+//grabs lat and lon of city
+function lonLatLocation(city) {
+  console.log(city);
+  var urlLatLon =
+    "http://api.openweathermap.org/geo/1.0/direct?q=" +
+    city +
+    "&limit=5&appid=569d785adfe9b44db482c835162b2e7a";
+  console.log(urlLatLon);
+  fetch(urlLatLon)
+    .then(function (response) {
+      console.log(response);
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+      city = data[0];
+      cityLat = city.lat;
+      cityLon = city.lon;
+      fetchWeather(cityLat, cityLon);
+    });
+}
 //form submission function
 function fetchWeather(city) {
   console.log(city);
@@ -12,5 +32,5 @@ function fetchWeather(city) {
 formButton.click(function (event) {
   event.preventDefault();
   input = userInput.val();
-  fetchWeather(input);
+  lonLatLocation(input);
 });
